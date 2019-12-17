04b1fd9b22f86520c77b0debecb560684356e9bc
shiny <shiny.gao@foxmail.com>
2019-12-17 14:00:38 +0800

    滚动到指定元素
    Signed-off-by: shiny <shiny.gao@foxmail.com>

--- main.js
+++ main.js
@@ -34,3 +34,14 @@ for (let i = 0; i < liTags.length; i++) {
         x.currentTarget.classList.remove('active')
     }
 }
+let aTags = document.querySelectorAll('nav.menu >ul >li >a')
+for (let i = 0; i < aTags.length; i++) {
+    aTags[i].onclick = function (x) {
+        x.preventDefault()
+        let a = x.currentTarget
+        let href = a.getAttribute('href')
+        let element = document.querySelector(href)
+        let top = element.offsetTop
+        window.scrollTo(0, top - 80)
+    }
+}
